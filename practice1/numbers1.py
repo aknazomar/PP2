08076610f1eb9c525b294b17a6ca8b093c087703
aknazomar <aknazomar@gmail.com>
2026-01-30 12:18:38 +0500

    numbers

---
+++ practice1/numbers1.py
@@ -0,0 +1,20 @@
+a = 1 #int
+b = 2.5 #float
+c = 1j #complex
+
+#to verify the type of variable we use type() function
+print(type(a))
+print(type(b))
+print(type(c))
+
+#converting number types 
+x = float(a)
+y = int(b)
+z = complex(a)
+
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
