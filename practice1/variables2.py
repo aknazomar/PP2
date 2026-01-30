7d5948142a0fffc3e4ab50d61a9e943d6e7bf015
aknazomar <aknazomar@gmail.com>
2026-01-30 12:07:24 +0500

    variables2 and 3

--- practice1/variables2.py
+++ practice1/variables2.py
@@ -4,3 +4,13 @@ _my_var = "John"
 myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
