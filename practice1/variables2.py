593d732cd2fafd45b9450826810946f9d6af0ded
aknazomar <aknazomar@gmail.com>
2026-01-30 12:12:07 +0500

    variables2 and 3, 4, 5

--- practice1/variables2.py
+++ practice1/variables2.py
@@ -1,3 +1,4 @@
+# Variable names
 myvar = "John"
 my_var = "John"
 _my_var = "John"
@@ -5,21 +6,37 @@ myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
 
+
+# typing many variables in the same line
 x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
 
+
+# giving same value for many varibles
 x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
 
+
+# joining many varibles
 x = "Python "
 y = "is "
 z = "awesome"
 print(x + y + z)
 
+
+# Multiple variables in one line
 x = 5
 y = "John"
 print(x, y)
+
+# Global variable
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
