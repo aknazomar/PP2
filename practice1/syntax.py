ed8ee5e5d75d7e9fea2654e04b149c4aeca4fc4b
aknazomar <aknazomar@gmail.com>
2026-02-06 13:16:40 +0500

    syntax 1 + 4 examples added

--- practice1/syntax.py
+++ practice1/syntax.py
@@ -3,14 +3,39 @@ a = int(input())
 b = int(input())
 if a > b:
     print("a is greater than b")
+elif a == b:
+    print("equal")
 else:
     print("a is not greater than b")
 
 
 #2 example
-u = 'english'
-b = 'discrete math'
-if u == "english":
-    print("yaay, english")
+c = input("c =").split()
+d = input("d =").split()
+if c > d:
+    print("c is longer than d")
+elif c == d:
+    print("equal")
 else:
-    print("wow")
+    print("d is longer than c")
+
+
+# 3 example
+a = "aknaz"
+o = "omar"
+if a == "aknaz" and o == "omar":
+    print("aknaz omar")
+
+#4 example 
+f = "faza"
+k = "kama"
+if f == "faza" or k == "kama":
+    print("faza i kama smotryat serial v studii")
+
+#5 example 
+arthouse = len("almaloud")
+moments = len("helping")
+if arthouse == 8:
+    print("yayayyayay")
+else:
+    print("oyoyoyoy")
