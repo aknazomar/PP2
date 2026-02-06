6dde7fdb777721ce751065bc6f70d2f666c1a7ff
aknazomar <aknazomar@gmail.com>
2026-02-06 21:30:08 +0500

    booleans

---
+++ practice2/Booleans_as_comparison_results.py
@@ -0,0 +1,21 @@
+#1
+print(5 == 5)   # True
+print(5 == 3)   # False
+
+#2
+print(10 != 7)  # True
+print(10 != 10) # False
+
+#3
+print(8 > 3)    # True
+print(2 < 1)    # False
+
+#4
+x = 15
+print(x > 10 and x < 20)  # True (оба условия выполняются)
+print(x > 20 or x == 15)  # True (второе условие выполняется)
+
+#5
+print("apple" == "apple")   # True
+print("apple" < "banana")   # True (сравнение по алфавиту)
+print("cat" > "dog")        # False
