be31b46c9cdd48ca4f0f3f7874d81396da4fda96
aknazomar <aknazomar@gmail.com>
2026-02-24 23:21:19 +0500

    lambda upd

--- practice3/lambda/lambda_with_sorted.py
+++ practice3/lambda/lambda_with_sorted.py
@@ -8,4 +8,12 @@ words = ["apple", "pie", "banana", "cherry"]
 sorted_words = sorted(words, key=lambda x: len(x))
 print(sorted_words)
 
-# 3
+# 3 (lambda with sorted) sorting numbers
+random_nums = [1, 2, 5, 7, 4, 3, 7, 9]
+sorted_nums = sorted(random_nums, key=lambda x: x)
+print(sorted_nums)
+
+# 4 (lambda with sorted) sorting algebraic operations 
+algebra = [1+2, 4*2, 1-1, 5+2, 9/1, 8-7]
+sorting = sorted(algebra, key=lambda x: x)
+print(sorting)
